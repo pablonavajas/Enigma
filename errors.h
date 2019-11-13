02607dba1fdad622d138232ab6cdcf06f8c382b5
Pablo Navajas Helguero <pn319@graphic12.doc.ic.ac.uk>
2019-11-13 21:16:53 +0000

    Updated errors.h file

--- errors.h
+++ errors.h
@@ -1,4 +1,6 @@
-/* error codes  */
+#ifndef ERRORS_H
+#define ERRORS_H
+
 #define INSUFFICIENT_NUMBER_OF_PARAMETERS         1
 #define INVALID_INPUT_CHARACTER                   2
 #define INVALID_INDEX                             3
@@ -11,3 +13,5 @@
 #define INCORRECT_NUMBER_OF_REFLECTOR_PARAMETERS  10
 #define ERROR_OPENING_CONFIGURATION_FILE          11
 #define NO_ERROR                                  0
+
+#endif
